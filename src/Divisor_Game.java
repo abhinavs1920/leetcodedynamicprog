0a8855fc338ed49bde3ded8388d505f01584c069
abhinavs1920 <113504774+abhinavs1920@users.noreply.github.com>
2023-01-04 11:25:48 +0530

    undone

---
+++ src/Divisor_Game.java
@@ -0,0 +1,12 @@
+public class Divisor_Game {
+    public static void main(String[] args) {
+
+    }
+    public boolean divisorGame(int n) {
+        boolean answer = true;
+
+
+
+        return answer;
+    }
+}
